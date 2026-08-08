ab21fff927b01d5af76d31adf17e0397295a270b
Manuel Schneider <manuelschneid3r@gmail.com>
2026-08-08 16:39:41 +0200

    feat: Add italian  translations

---
+++ i18n/docs_it.ts
@@ -0,0 +1,102 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>ConfigWidget</name>
+    <message>
+        <source>Cancel</source>
+        <translation>Annulla</translation>
+    </message>
+    <message>
+        <source>Update</source>
+        <translation>Aggiorna</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <source>Downloading docset list from &apos;%1&apos;</source>
+        <translation>Download dell&apos;elenco docset da &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <source>Error fetching docset list: %1</source>
+        <translation>Errore nel recupero dell&apos;elenco docset: %1</translation>
+    </message>
+    <message>
+        <source>Docset list updated.</source>
+        <translation>Elenco docset aggiornato.</translation>
+    </message>
+    <message>
+        <source>Failed to save fetched docset list: %1</source>
+        <translation>Impossibile salvare l&apos;elenco docset recuperato: %1</translation>
+    </message>
+    <message>
+        <source>Failed to parse docset list: %1</source>
+        <translation>Impossibile analizzare l&apos;elenco docset: %1</translation>
+    </message>
+    <message>
+        <source>Downloading docset from &apos;%1&apos;</source>
+        <translation>Download docset da &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <source>Extracting file &apos;%1&apos;</source>
+        <translation>Estrazione del file &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <source>Searching docset in &apos;%1&apos;</source>
+        <translation>Ricerca docset in &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <source>Renaming &apos;%1&apos; to &apos;%2&apos;</source>
+        <translation>Rinomina &apos;%1&apos; in &apos;%2&apos;</translation>
+    </message>
+    <message>
+        <source>Docset &apos;%1&apos; ready.</source>
+        <translation>Docset &apos;%1&apos; pronto.</translation>
+    </message>
+    <message>
+        <source>Failed renaming dir &apos;%1&apos; to &apos;%2&apos;.</source>
+        <translation>Impossibile rinominare la directory &apos;%1&apos; in &apos;%2&apos;.</translation>
+    </message>
+    <message>
+        <source>Failed finding extracted docset in %1</source>
+        <translation>Impossibile trovare il docset estratto in %1</translation>
+    </message>
+    <message>
+        <source>Extracting docset failed: &apos;%1&apos; (%2)</source>
+        <translation>Estrazione del docset non riuscita: &apos;%1&apos; (%2)</translation>
+    </message>
+    <message>
+        <source>Failed to write to file: &apos;%1&apos;</source>
+        <translation>Impossibile scrivere nel file: &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <source>Failed creating temporary directory</source>
+        <translation>Impossibile creare la directory temporanea</translation>
+    </message>
+    <message>
+        <source>Cancelled &apos;%1&apos; docset download.</source>
+        <translation>Download docset &apos;%1&apos; annullato.</translation>
+    </message>
+    <message>
+        <source>Directory removed &apos;%1&apos;</source>
+        <translation>Directory rimossa &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <source>Failed to remove directory &apos;%1&apos;</source>
+        <translation>Impossibile rimuovere la directory &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <source>Open documentation</source>
+        <translation>Apri documentazione</translation>
+    </message>
+    <message>
+        <source>Remove docset &apos;%1&apos;?</source>
+        <translation>Rimuovere il docset &apos;%1&apos;?</translation>
+    </message>
+    <message>
+        <source>Download finished.</source>
+        <translation>Download completato.</translation>
+    </message>
+</context>
+</TS>
